88df95ba3707ad2194db9e121dd2ea46ce005763
padale323 <padale323@gmail.com>
2026-08-01 15:13:20 -0400

    Refactor print statement into an IIFE

--- Official/helper/plugin.js
+++ Official/helper/plugin.js
@@ -1,1 +1,3 @@
-print("my friend is not on discord and i need him to get on because there is an emergency so i am not re-adding the plugin until then :)");
+(function () {
+    print("SHADOWBYTE-PLUGIN: my friend is not on discord and i need him to get on because there is an emergency so i am not re-adding the plugin until then :)");
+})();
